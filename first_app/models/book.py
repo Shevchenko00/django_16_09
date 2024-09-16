57920cc6fd869f98ce856c112d7c85bf0611e2b0
ShevchenkoO <shevchenko.sasha2005@gmail.com>
2024-09-16 12:39:25 +0200

    update

--- first_app/models/book.py
+++ first_app/models/book.py
@@ -2,12 +2,17 @@ from django.db import models
 from django.db.models import UniqueConstraint, Q
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
     registered = models.BooleanField(null=True)
     managed = models.BooleanField(null=True)
+    authors = models.ForeignKey(Author, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.title} написано {self.author}"
